e5d4113b87dbcdded67d1d8dcfa48877b3b0a23e
Lucas <lucas.carlier.2001@gmail.com>
2022-03-17 22:47:11 +0100

    getAllTweet + addTweet

--- src/repositories/TwitterCloneRepository.js
+++ src/repositories/TwitterCloneRepository.js
@@ -2,13 +2,34 @@ import axios from "axios";
 
 export default
     {
-        getAllTweets() {
-            var variable;
-            return axios.get("https://fges-twitter-clone.herokuapp.com/allTweets", {
+        async getAllTweets() {
+            let tweets = await (
+                axios.get("https://fges-twitter-clone.herokuapp.com/allTweets", {
                 headers: {
                     "x-fges-user-key": "zD8CAcUtB2KlLPcBXX9j96twyxyLJFaX",
                 },
-            }).then(response => variable = response.data)
-            .catch(response => variable = response.error);
+            }));
+            console.log(tweets)
+            return tweets.data;
+        },
+
+        async getTweet(id)
+        {
+            let tweet = await(axios.get("https://fges-twitter-clone.herokuapp.com/tweet/" + id, {
+                headers: {
+                    "x-fges-user-key": "zD8CAcUtB2KlLPcBXX9j96twyxyLJFaX",
+                },
+            }));
+            return tweet.data;
+        },
+
+        async addTweet(tweet)
+        {
+            let tweetSave = await (axios.post("https://fges-twitter-clone.herokuapp.com/tweet",tweet, {
+                headers: {
+                    "x-fges-user-key": "zD8CAcUtB2KlLPcBXX9j96twyxyLJFaX",
+                }
+            }));
+            return tweetSave.data;
         }
     }
