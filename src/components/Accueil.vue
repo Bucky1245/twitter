2a049261d71fa7ed60355efa4454e8725a0ab0ef
Lucas <lucas.carlier.2001@gmail.com>
2022-03-17 22:47:51 +0100

    ajout du composant addTweet + affichage

--- src/components/Accueil.vue
+++ src/components/Accueil.vue
@@ -1,16 +1,19 @@
 <template>
-    <ul>
-        <li v-for="tweet in tweets" :key="tweet.id" >
-        {{ tweet.value }} 
+    <ul v-if="tweets.length > 0">
+        <li v-for="tweet in tweets" :key="tweet._id" >
+        {{ tweet.message }} <router-link :to='"/tweet/" + tweet._id'>lien</router-link> <router-link :to='"/profile/" + tweet.owner'>le poto</router-link>
         </li>
     </ul>
+    <add-tweet></add-tweet>
 </template>
 
 <script>
 import { defineComponent } from "@vue/runtime-core";
 import TwitterCloneRepository from "../repositories/TwitterCloneRepository";
+import addTweet from "./addTweet.vue";
 
 export default defineComponent({
+    components:{addTweet},
     data: () => {
         return {
             tweets: [],
